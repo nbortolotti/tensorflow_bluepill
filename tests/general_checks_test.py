106c05f3562d8e4e78baf5457b3b84ddef7d6b41
Nicolas Bortolotti <nbortolotti@gmail.com>
2019-03-26 18:48:49 +0100

    adding test for scalars functions, add modifications into main readme

--- tests/general_checks_test.py
+++ tests/general_checks_test.py
@@ -6,6 +6,7 @@ from unittest import TestCase
 sys.path.insert(0, os.path.abspath('.'))
 
 from device_check import measure
+from prescription_one.tensor_scalars import add_pill
 
 
 class GeneralDeviceCheck(TestCase):
@@ -15,3 +16,13 @@ class GeneralDeviceCheck(TestCase):
 
         result = measure(tf.random.normal(t), steps)
         self.assertLess(result, 10.)
+
+
+class general_scalars(TestCase):
+    def test_add(self):
+        x = 3
+        y = 5
+
+        result = add_pill(x, y)
+
+        self.assertTrue(tf.assert_equal(result, 8))
