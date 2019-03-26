c62923d9e604f3cd7fb7d1a409aabd7e7de8f475
Nicolas Bortolotti <nbortolotti@gmail.com>
2019-03-26 23:02:20 +0100

    initial version ragged tensors issue #1

--- prescription_one/tensors_type.py
+++ prescription_one/tensors_type.py
@@ -4,6 +4,11 @@ import tensorflow as tf
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+
+def ragged_add_pill(a, b):
+    return tf.add(a, b)
+
+
 if __name__ == '__main__':
     pa = argparse.ArgumentParser(description='tensorflow tensor types')
     pa.add_argument('--operation', dest='operation', required=True, help='operation')
@@ -11,8 +16,13 @@ if __name__ == '__main__':
 
     args = pa.parse_args()
 
-    s = tf.SparseTensor(indices=[[0, 1], [1, 0], [2, 3]],
-                        values=[1., 2., 3.],
-                        dense_shape=[3, 4])
+    # s = tf.SparseTensor(indices=[[0, 1], [1, 0], [2, 3]],
+    #                     values=[1., 2., 3.],
+    #                     dense_shape=[3, 4])
+    #
+    # print(s)
 
-    print(s)
+    numbers_a = tf.ragged.constant([[3, 5], [], [7, 9, 1], [5], []])
+    numbers_b = tf.ragged.constant([[1, 1], [], [1, 1, 1], [1], []])
+
+    print(ragged_add_pill(numbers_a, numbers_b))
